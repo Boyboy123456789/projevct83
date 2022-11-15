a0a1949fb970d245816d034c80822ffb83ff11dc
Boyboy123456789 <110863816+Boyboy123456789@users.noreply.github.com>
2022-11-15 17:53:50 -0600

    Update C83_Template.js

--- C83_Template.js
+++ C83_Template.js
@@ -1,8 +1,8 @@
 var images = ["https://i.postimg.cc/MKdhy06Z/family.jpg","https://i.postimg.cc/L6HcYQM0/grand-father.png", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/bw5W5zSK/mother.jpg", "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg"];
 
-var names = ["Family Book","Ranbir Singh - Grandfather", "Diljeet Singh - Father", "Alia Singh - Mother", "Rocky Singh - Brother", "Soni Singh - Sister"];
+var names = ["Family Book","rohan - Grandfather", "kashyap - Father", "seema - Mother", "aarav - Brother"];
 
-var age = [60, 40, 60, 30, 25, 20]
+var age = [72, 44, 45, 12]
 
 /*
 
@@ -30,7 +30,7 @@ function update()
 {
     i++;
 
-    var numbers_of_family_member_in_array = 5
+    var numbers_of_family_member_in_array = 4
     if(i > numbers_of_family_member_in_array )
     {
         i = 0;
